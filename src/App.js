8c5ec39d2aeb6133e2654544a54125044356dc61
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-06-18 00:52:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()  => {
+
+    const [youtubePlayerReady, setYoutubePlayerReady] = useState(false)
+    const [startVideo, setStartVideo] = useState(0)
+    const [endVideo, setEndVideo] = useState(null)
+
+    useEffect(() => {
+        if (!window.YT) {
+            loadYT()
+            window.onYouTubeIframeAPIReady = onYouTubePlayerAPIReady
+        } else {
+            onYouTubePlayerAPIReady(window.YT)
+        }
+    })
+
+    const loadYT = async () => {
+        const tag = document.createElement('script');
+        tag.src = await "https://www.youtube.com/player_api"
+        const firstScriptTag = document.getElementsByTagName('script')[0]
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+    }
+
+    const onYouTubePlayerAPIReady = () => {
+        const player = new window.YT.Player('ytplayer', {
+            videoId: 'M7lc1UVf-VE',
+            events: {
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange
+            },
+        });
+    }
+
+    const onPlayerStateChange = (event) => {
+        console.log('youtube state change');
+        console.log('Is playing');
+        console.log(event.data == window.YT.PlayerState.PLAYING);
+        console.log('playing seconds');
+        console.log(event.target.playerInfo.currentTime);
+        console.log('playing total');
+        console.log(event.target.playerInfo.duration);
+
+    }
+
+    const onPlayerReady = (event) => {
+        event.target.playVideo();
+        setYoutubePlayerReady(true)
+
+    };
+
+    return (
+        <div className="App">
+            youtube player
+            <div id="ytplayer"></div>
+        </div>
+    )
 }
 
 export default App;
