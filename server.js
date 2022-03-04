63367dbedf634b996252dd696b8fbf370bee61b6
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-03-04 01:07:04 -0400

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://youtube-house-party.herokuapp.com'
     }
 })
 
