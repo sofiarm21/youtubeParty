aa733070277ed4d9c2295d1ae65121a21796d66e
Sofía Rodríguez <sofiaromorales@gmail.com>
2022-05-29 23:34:53 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import './styles/index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+import './styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
